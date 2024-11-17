534849a09759d40db53a68a3d57781ad509829f8
okoli813 <chidiebube.okoli@binghamuni.edu.ng>
2024-11-17 06:14:17 -1100

    Add files via upload

---
+++ cos101.py
@@ -0,0 +1,45 @@
+
+
+# def name_of_functions():
+#    input('area of a triangle')
+
+#name_of_functions():
+
+def calculate_velocity():
+    input("displacement*time")
+
+calculate_velocity()
+
+# ASSIGNMENT
+
+def a():
+    # pressure = "Force/Area"
+    force=int(input("what is the force applied?"))
+    area=int(input("what is the area covered?"))
+    print(force/area)
+
+def b():
+    # volume ="length * breadth * height"
+    length=int(input("what is the length measured"))
+    breadth=int(input("what is the breadth measured"))
+    height=int(input("what is the height measured"))
+    print(length * breadth * height)
+
+
+def c():
+    # displacement = "velocity * time"
+    velocity=int(input("what is the velocity travelled?"))
+    time=int(input("what is the time taken?"))
+    print(velocity*time)
+
+def d():
+    # impulse = "Force * time"
+    force = int(input("what is the force applied?"))
+    time = int(input("what is the time taken?"))
+    print(force * time)
+
+ def e():
+     # work_done = "distance * time"
+     distance = int(input("what is the distance travelled?"))
+     time = int(input("what is the time taken?"))
+     print(distance * time)
