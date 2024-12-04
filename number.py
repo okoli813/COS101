bd58c0db91be6f19a0b1f284b38ef1a7a00a4d27
okoli813 <chidiebube.okoli@binghamuni.edu.ng>
2024-12-04 10:17:54 -1100

    Add files via upload

---
+++ number.py
@@ -0,0 +1,11 @@
+# number = [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+# print(sum(number))
+# total = 0
+# for number in numbers:
+#     total = total + number
+#     print(total)
+number = [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10,]
+
+reversed_numbers = number [::-1]
+print(reversed_numbers)
+
